015b37a618f11c9ebbc25d94eb2418d0ec1395c1
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-01-05 12:22:35 +0100

    Add combinated units, new README, python3

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ Unit conversion which reads data from file table.txt
 
 ### Usage:
 - Enter unit data into table.txt file (for example 1 m = 100 cm)
-- Run main.py 
+- Run main.py with python3 
 - Follow instructions printed on screen
+
+### Eample conversions with current table.txt file:
+- 1 m = 3.28084035433 ft
+- 5 ft/sec = 5486.399231905292 m/h
+- 5 A = 5 C/sec
+- 5 A = 18000.0 C/h
--- main.py
+++ main.py
@@ -6,136 +6,162 @@ lines = read_file.readlines()
 
 numbers = ['1','2','3','4','5','6','7','8','9','0','.']
 units = {}
+units2 = {}
 available_units = []
 for l in lines:
-	if l == '\n':
-		break
-	i=0
-	number1=""
-	number2=""
-	unit1=""
-	unit2=""
+        if l == '\n':
+                break
+        i=0
+        number1=""
+        number2=""
+        unit1=""
+        unit2=""
 
-	while(l[i] in numbers):
-		number1+=l[i]
-		i+=1
-	if(l[i]==' '):
-		i+=1
-	while(l[i] != " " and l[i] != '='):
-		unit1+=l[i]
-		i+=1
-	while(l[i] == ' ' or l[i] == '='):
-		i+=1
-	while(l[i] in numbers):
-		number2+=l[i]
-		i+=1
-	if(l[i]==' '):
-		i+=1
-	while(i < len(l) and l[i] != '\n'):
-		unit2+=l[i]
-		i+=1
-
-	number2 = float(number2) / float(number1)
-	
-	if unit1 not in units:
-		units[unit1] = [[unit2, number2]]
-	else:
-		units[unit1].append([unit2, number2])
-
-	if unit2 not in units:
-		units[unit2] = [[unit1, 1/number2]]
-	else:
-		units[unit2].append([unit1, 1/number2])
-
-	available_units.append(unit1)
-	available_units.append(unit2)
+        while(l[i] in numbers):
+                number1+=l[i]
+                i+=1
+        if(l[i]==' '):
+                i+=1
+        while(l[i] != " " and l[i] != '='):
+                unit1+=l[i]
+                i+=1
+        while(l[i] == ' ' or l[i] == '='):
+                i+=1
+        while(l[i] in numbers):
+                number2+=l[i]
+                i+=1
+        if(l[i]==' '):
+                i+=1
+        if('/' not in l):
+                while(i < len(l) and l[i] != '\n'):
+                        unit2+=l[i]
+                        i+=1
+                number2 = float(number2) / float(number1)
+                if unit1 not in units:
+                        units[unit1] = [[unit2, number2]]
+                else:
+                        units[unit1].append([unit2, number2])
+                if unit2 not in units:
+                        units[unit2] = [[unit1, 1/number2]]
+                else:
+                        units[unit2].append([unit1, 1/number2])
+                available_units.append(unit1)
+                available_units.append(unit2)
+        else:
+                while(i < len(l) and l[i] != '\n'):
+                        unit2+=l[i]
+                        i+=1
+                number2 = float(number2) / float(number1)
+                units2[unit1] = [unit2, number2]
+                units2[unit2] = [unit1, 1/number2]
+                available_units.append(unit1)
+                available_units.append(unit2.split('/')[0])
+                available_units.append(unit2.split('/')[1])
 
 def bfs_shortest_path(u1, u2):
-	visited = [u1]
-	queue = [(u1, [])]
-	while queue:
-		v, path = queue.pop(0)
-		visited.append(v)
-		for n in units[v]:
-			if n[0] == u2:
-				return path + [v,n[0]]
-			if n[0] in visited:
-				continue
-			queue.append((n[0], path + [v]))
-			visited.append(n[0])
-	return None
+        if(u1==u2):
+                return "same"
+        visited = [u1]
+        queue = [(u1, [])]
+        while queue:
+                v, path = queue.pop(0)
+                visited.append(v)
+                for n in units[v]:
+                        if n[0] == u2:
+                                return path + [v,n[0]]
+                        if n[0] in visited:
+                                continue
+                        queue.append((n[0], path + [v]))
+                        visited.append(n[0])
+        return None
+def check(u1,am,u2):
+        if(u1==u2):
+                print(am)
+                return True
+        return False
+def single(u1,am,u2):
+        if(u1 not in available_units or u2 not in available_units):
+                print("Unit(s) not in table!")
+                return 0
 
-def single():
+        path = bfs_shortest_path(u1,u2)
 
-	u1 = raw_input("Enter unit to convert from: ")
-	am = int(raw_input("Enter amount of that unit: "))
-	u2 = raw_input("Enter unit to convert to: ")
+        if path == None:
+                print("Conversion not possible")
+                return 0
 
-	if(u1 not in available_units or u2 not in available_units):
-		print("Unit(s) not in table!")
-		return 0
-	#if u1 == u2:
-	#	print(am)
-	#	return 0
+        x = am
+        if path == "same":
+                print(x)
+                return 0
+        for i in range(len(path)-1):
+                curr = path[i]
+                for u in units[curr]:
+                        if(u[0] == path[i+1]):
+                                x = x * u[1]
+                                break
+        print(x)
 
-	path = bfs_shortest_path(u1,u2)
+def multi(u1,am,u2):
+        u11 = u1.split('/')[0]
+        u12 = u1.split('/')[1]
 
-	if path == None:
-		print("Conversion not possible")
-		return 0
+        u21 = u2.split('/')[0]
+        u22 = u2.split('/')[1]
 
-	x = am
-	for i in range(len(path)-1):
-		curr = path[i]
-		for u in units[curr]:
-			if(u[0] == path[i+1]):
-				x = x * u[1]
-				break
-	print(x)
+        if(u11 not in available_units or u12 not in available_units or u21 not in available_units or u22 not in available_units):
+                print("Unit(s) not in table!")
+                return 0
 
-def multi():
-	u1 = raw_input("Enter unit to convert from (use '/' to separate): ")
-	am = int(raw_input("Enter amount of that unit: "))
-	u2 = raw_input("Enter unit to convert to (use '/' to separate): ")
+        path1 = bfs_shortest_path(u11,u21)
+        path2 = bfs_shortest_path(u12,u22)
+        if(path1 == None or path2 == None):
+                print("Conversion not possible")
+                return 0
 
-	u11 = u1.split('/')[0]
-	u12 = u1.split('/')[1]
+        x1 = 1
+        if(path1!="same"):
+                for i in range(len(path1)-1):
+                        curr = path1[i]
+                        for u in units[curr]:
+                                if(u[0] == path1[i+1]):
+                                        x1 = x1 * u[1]
+                                        break
+        x2 = 1
+        if(path2!="same"):
+                for i in range(len(path2)-1):
+                        curr = path2[i]
+                        for u in units[curr]:
+                                if(u[0] == path2[i+1]):
+                                        x2 = x2 * u[1]
+                                        break
 
-	u21 = u2.split('/')[0]
-	u22 = u2.split('/')[1]
-
-	if(u11 not in available_units or u12 not in available_units or u21 not in available_units or u22 not in available_units):
-		print("Unit(s) not in table!")
-		return 0
-
-	path1 = bfs_shortest_path(u11,u21)
-	path2 = bfs_shortest_path(u12,u22)
-	if(path1 == None or path2 == None):
-		print("Conversion not possible")
-		return 0
-
-	x1 = 1
-	for i in range(len(path1)-1):
-		curr = path1[i]
-		for u in units[curr]:
-			if(u[0] == path1[i+1]):
-				x1 = x1 * u[1]
-				break
-	x2 = 1
-	for i in range(len(path2)-1):
-		curr = path2[i]
-		for u in units[curr]:
-			if(u[0] == path2[i+1]):
-				x2 = x2 * u[1]
-				break
-
-	print(am * x1 / x2)
+        print(am * x1 / x2)
 
 
-inp = raw_input("Single or complex conversion?(s/c): ")
-if(inp == 's'):
-	single()
-elif(inp == 'c'):
-	multi()
+u1 = input("Enter unit to convert from (use '/' to separate): ")
+am = int(input("Enter amount of that unit: "))
+u2 = input("Enter unit to convert to (use '/' to separate): ")
+if('/' in u1 and '/' in u2):
+        multi(u1,am,u2)
+elif('/' not in u1 and '/' not in u2):
+        single(u1,am,u2)
 else:
-	print("Type 's' or 'c'!")
+        if('/' in u1 and u1 in units2):
+                if(not check(units2[u1][0],units2[u1][1]*am,u2)):
+                        single(units2[u1][0],units2[u1][1]*am,u2)
+        elif('/' not in u1 and u1 in units2):
+                if(not check(units2[u1][0],units2[u1][1]*am,u2)):
+                        multi(units2[u1][0],units2[u1][1]*am,u2)
+        elif('/' in u2 and u2 in units2):
+                if(not check(u1,units2[u2][1]*am,units2[u2][0])):
+                        single(u1,units2[u2][1]*am,units2[u2][0])
+        elif('/' not in u2 and u2 in units2):
+                if(not check(u1,units2[u2][1]*am,units2[u2][0])):
+                        multi(u1,units2[u2][1]*am,units2[u2][0])
+        else:
+                print("Error!")
+                
+                
+                
+        
